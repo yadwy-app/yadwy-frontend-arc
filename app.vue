d48bed0cc0c15c9af2e9d103afbff921c315f030
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-08-05 11:42:21 +0300

    add i18n & tailwind & shadcn ui

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { locale, setLocale } = useI18n()
+</script>
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <div>
+      <div>
+        <Button @click="setLocale('en')">en</Button>
+        <Button @click="setLocale('ar')">AR</Button>
+        <p>{{ $t('welcome') }}</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ i18n.config.ts
@@ -0,0 +1,11 @@
+import en from "./locales/en.json"
+import ar from "./locales/ar.json"
+
+export default {
+    legacy: false,
+    locale: 'en',
+    messages: {
+        en,
+        ar
+    }
+}
