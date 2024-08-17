fa74a9ada1ddcb0c144ad923a34580880616cf8b
0xyosef <yosef2020go@gmail.com>
2024-08-18 00:33:30 +0300

    add : add google fonts Lexend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,31 @@
 import { resolve } from "node:path";
 
 export default defineNuxtConfig({
-	compatibilityDate: "2024-04-03",
-	devtools: { enabled: true },
-	modules: [
-		"@nuxtjs/tailwindcss",
-		"shadcn-nuxt",
-		"@nuxtjs/i18n",
-		"@nuxt/icon",
-		"@nuxt/image",
-	],
-	shadcn: {
-		prefix: "",
-		componentDir: "./components/ui",
-	},
-	i18n: {
-		defaultLocale: "ar",
-		locales: ["en", "ar"],
-	},
-	alias: {
-		"@": resolve(__dirname, "./"),
-	},
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/i18n",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxtjs/google-fonts",
+  ],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+  i18n: {
+    defaultLocale: "ar",
+    locales: ["en", "ar"],
+  },
+  alias: {
+    "@": resolve(__dirname, "./"),
+  },
+  googleFonts: {
+    families: {
+      Lexend: [400, 600, 700],
+    },
+    display: "swap",
+  },
 });
